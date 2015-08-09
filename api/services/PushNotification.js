f383762ff2d5c11f77315c00b8cfcc65eece72b0
jaumard <ioquoo3H>
2015-08-09 20:32:54 +0200

    Implement push GCM

--- api/services/PushNotification.js
+++ api/services/PushNotification.js
@@ -2,9 +2,22 @@
  * Created by jaumard on 09/08/2015.
  */
 module.exports = {
+	/**
+	 * Send a push message to one or more devices
+	 * @param ids id or array of id to send push message
+	 * @param messageInfos parameter and message to send see https://github.com/ToothlessGear/node-gcm#usage for more information
+	 * @param retry when message fail to send
+	 * @param next callback
+	 */
 	sendGCMNotification : function (ids, messageInfos, retry, next)
 	{
 		var gcm = require('node-gcm');
+		var _ = require('lodash');
+
+		if (!Array.isArray(ids))
+		{
+			ids = [ids];
+		}
 
 		ids = _.chunk(ids, 1000);
 
@@ -47,9 +60,16 @@ module.exports = {
 			}
 		}
 	},
+	/**
+	 * Send a push message to one or more Apple devices
+	 * @param token or array or tokens to send push message
+	 * @param message object to send, more information on parameters here https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW1
+	 * TODO not tested yet !
+	 */
 	sendAPNNotification : function (token, message)
 	{
 		var apn     = require('apn');
+		var _ = require('lodash');
 		var options = sails.config.push.apn;
 
 		if (!Array.isArray(token))
@@ -60,31 +80,7 @@ module.exports = {
 		var apnConnection = new apn.Connection(options);
 
 		var note = new apn.Notification();
-
-		if (message.expiry)
-		{
-			note.expiry = message.expiry;
-		}
-		if (message.badge)
-		{
-			note.badge = message.badge;
-		}
-		if (message.badge)
-		{
-			note.badge = message.badge;
-		}
-		if (message.sound)
-		{
-			note.sound = message.sound;
-		}
-		if (message.alert)
-		{
-			note.alert = message.alert;
-		}
-		if (message.payload)
-		{
-			note.payload = message.payload;
-		}
+		note  = _.merge(note, message);
 
 		for (var i = 0; i < token.length; i++)
 		{
@@ -92,6 +88,5 @@ module.exports = {
 			var myDevice = new apn.Device(obj);
 			apnConnection.pushNotification(note, myDevice);
 		}
-
 	}
 };
