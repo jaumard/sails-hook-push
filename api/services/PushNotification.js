003614aa68ac5c04f9c3b82e46caec37f7f106db
jaumard <ioquoo3H>
2015-08-09 14:00:15 +0200

    Implement push GCM

--- api/services/PushNotification.js
+++ api/services/PushNotification.js
@@ -1,4 +1,97 @@
 /**
  * Created by jaumard on 09/08/2015.
  */
-module.exports = {}
+module.exports = {
+	sendGCMNotification : function (ids, messageInfos, retry, next)
+	{
+		var gcm = require('node-gcm');
+
+		ids = _.chunk(ids, 1000);
+
+		var message;
+		if (messageInfos)
+		{
+			message = new gcm.Message(messageInfos);
+		}
+		else
+		{
+			message = new gcm.Message();
+		}
+		// Set up the sender with you API key
+		var sender = new gcm.Sender(sails.config.push.gcm.senderId);
+
+		for (var i = 0; i < ids.length; i++)
+		{
+			var obj = ids[i];
+
+			if (retry === true)
+			{
+				sender.send(message, obj, function (err, result)
+				{
+					next(err, result);
+				});
+			}
+			else if (retry === false)
+			{
+				sender.sendNoRetry(message, obj, function (err, result)
+				{
+					next(err, result);
+				});
+			}
+			else
+			{
+				sender.send(message, obj, retry, function (err, result)
+				{
+					next(err, result);
+				});
+			}
+		}
+	},
+	sendAPNNotification : function (token, message)
+	{
+		var apn     = require('apn');
+		var options = sails.config.push.apn;
+
+		if (!Array.isArray(token))
+		{
+			token = [token];
+		}
+
+		var apnConnection = new apn.Connection(options);
+
+		var note = new apn.Notification();
+
+		if (message.expiry)
+		{
+			note.expiry = message.expiry;
+		}
+		if (message.badge)
+		{
+			note.badge = message.badge;
+		}
+		if (message.badge)
+		{
+			note.badge = message.badge;
+		}
+		if (message.sound)
+		{
+			note.sound = message.sound;
+		}
+		if (message.alert)
+		{
+			note.alert = message.alert;
+		}
+		if (message.payload)
+		{
+			note.payload = message.payload;
+		}
+
+		for (var i = 0; i < token.length; i++)
+		{
+			var obj      = token[i];
+			var myDevice = new apn.Device(obj);
+			apnConnection.pushNotification(note, myDevice);
+		}
+
+	}
+};
--- templates/push.js
+++ templates/push.js
@@ -5,4 +5,30 @@
  *
  */
 
-module.exports.push = {};
+module.exports.push = {
+	gcm : {
+		senderId : ""
+	},
+	apn : {
+		cert : "",
+		key  : ""
+		/*
+		 ca         : [],
+		 pfx        : "",
+		 passphrase : "",
+		 production : NODE_ENV == "production",
+		 voip : false,
+		 port : 2195,
+		 rejectUnauthorized : true,
+		 cacheLength : 1000,
+		 autoAdjustCache : true,
+		 maxConnections : 1,
+		 connectTimeout : 10000,
+		 connectionTimeout : 3600000,
+		 connectionRetryLimit : 10,
+		 buffersNotifications : true,
+		 fastMode : false
+		 //more infos here : https://github.com/argon/node-apn/blob/master/doc/connection.markdown
+		 */
+	}
+};
